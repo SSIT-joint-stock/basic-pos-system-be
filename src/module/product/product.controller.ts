6ace2b70f7b8fdbc30d0a3c6ff833da7103ccb5b
hoaha110405 <hoaha110404@gmail.com>
2025-10-06 11:56:45 +0700

    fix(develop): add feat create product batch in product module and docs

--- src/module/product/product.controller.ts
+++ src/module/product/product.controller.ts
@@ -139,4 +139,15 @@ export class ProductController {
   remove(@Param('storeId') storeId: string, @Param('id') id: string) {
     return this.productService.remove(storeId, id);
   }
+
+  @Post('invoice-create-product')
+  @RequirePermissions([PERMISSIONS.PRODUCT_CREATE])
+  @ApiSuccess('Create product successfully')
+  createProductsBatch(
+    @Param('storeId') storeId: string,
+    @UserWithPermissions() user: IUserWithPermissions,
+    @Body() items: CreateProductDto[],
+  ) {
+    return this.productService.createProductsBatch(storeId, user, items);
+  }
 }
