e6abf6ad3cb83d51906782468a6ed35ef3398e19
minhpham2409 <providminh24092004@gmail.com>
2025-12-28 00:19:42 +0700

    feat(store-member-excel): implement excel template and export for store member

--- src/module/store-member/store-member.module.ts
+++ src/module/store-member/store-member.module.ts
@@ -3,10 +3,22 @@ import { StoreMemberController } from './store-member.controller';
 import { StoreMemberService } from './store-member.service';
 import { PrismaService } from 'app/prisma/prisma.service';
 import { BcryptService } from 'app/common/helpers/bcrypt.util';
+import { StoreMemberExcelService } from './store-member-excel.service';
+import { ExcelTemplateService } from 'app/shared/excel-template/excel-template.service';
+import { Format } from 'app/common/helpers/format';
+import { FormatStatus } from 'app/common/helpers/status';
 
 @Module({
   controllers: [StoreMemberController],
-  providers: [StoreMemberService, PrismaService, BcryptService],
+  providers: [
+    StoreMemberService,
+    StoreMemberExcelService,
+    PrismaService,
+    BcryptService,
+    ExcelTemplateService,
+    Format,
+    FormatStatus,
+  ],
   exports: [StoreMemberService],
 })
 export class StoreMemberModule {}
